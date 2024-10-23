9e387ffd881369c13a9b6fd0b5dacca1af905cc3
Dale-Xu1 <dalexu1@outlook.com>
2024-10-23 11:20:16 -0400

    Updates

---
+++ _app/immutable/chunks/paths.36652f6e.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_rs3s7v)==null?void 0:s.base)??"/hsr-banner-history";var a;const t=((a=globalThis.__sveltekit_rs3s7v)==null?void 0:a.assets)??e;export{t as a,e as b};
---
+++ _app/immutable/nodes/2.b69445a2.js
@@ -0,0 +1,1 @@
+import{L as n}from"../chunks/layout.5a74d8df.js";const e=!0,o=Object.freeze(Object.defineProperty({__proto__:null,prerender:e},Symbol.toStringTag,{value:"Module"}));export{n as component,o as universal};
