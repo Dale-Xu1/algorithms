7f968c01e7e436a53769511d326ad4fa519d486d
Dale-Xu1 <dalexu1@outlook.com>
2024-10-23 11:22:03 -0400

    Deploy to github pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+import { base } from "$app/paths"
+</script>
+
 <svelte:head>
     <title>Algorithm Visualizations</title>
 </svelte:head>
 <div class="main">
     <h1>Algorithm Visualizations</h1>
-    <a href="/sorting">Sorting Algorithms</a>
-    <a href="/maze">Maze Generation Algorithms</a>
+    <a href="{base}/sorting">Sorting Algorithms</a>
+    <a href="{base}/maze">Maze Generation Algorithms</a>
 </div>
 <style>
 :global(*) {
